f17b36962fbccc6cad4cf2d6771e42c1af8c49c6
labunix <unix_lab@yahoo.co.jp>
2012-08-12 18:37:07 +0900

    iptables reconfigure,and modify sfwreport.sh

--- sfwreport.sh
+++ sfwreport.sh
@@ -1,27 +1,35 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 SFWLOG=/var/log/iptables.log
 SFWREP=/var/log/sfwreport.log
 SFWMAIL=root@`hostname -f`
 
-grep "iptables.* IN" "$SFWLOG" | sed s/"^.*iptables"/"iptables"/g | sed s/" "/"\n"/g
-  grep "IN=\|OUT=\|SRC=\|DST=\|PROTO=\|SPT\|DPT" | \
-  sed s/"iptables-in"/"-A -t filter INPUT "/g
-  sed s/"iptables-ou"/"-A -t filter OUTPUT "/g
-  sed s/"iptables-fw"/"-A -t filter FORWARD "/g
-  sed s/"iptables-npr"/"-A -t nat PREROUTING "/g
-  sed s/"iptables-npo"/"-A -t nat POSTROUTING "/g
-  sed s/"iptables-nou"/"-A -t nat OUTPUT "/g
+grep "iptables.* IN" "$SFWLOG" | \
+  sed s/"^\(.*\)\( iptables-[a-z]* IN=\)"/"\2"/g | \
+  sed s/" "/"\n"/g | \
+  grep "IN=\|OUT=\|SRC=\|DST=\|PROTO=\|SPT=\|DPT=\|TYPE=\|iptables" | \
+  sed s/'iptables-in'/' INPUT '/g | \
+  sed s/'iptables-ou'/' OUTPUT '/g | \
+  sed s/'iptables-fw'/' FORWARD '/g | \
   sed s/"IN=\$"//g | \
-  sed s/"IN="/'-i '/g | \
+  sed s/"IN="/' -i '/g | \
   sed s/"OUT=\$"//g | \
-  sed s/"OUT="/'-i '/g | \
-  sed s/"SRC="/'-s '/g | \
-  sed s/"DST="/'-d '/g | \
-  sed s/"PROTO="/'-p '//g | \
-  sed s/"SPT="/'--sport '/g | \
-  sed s/"DPT="/'--dport '/g | \
-  sed s/"^"/"\[0:0\] "//g | tee "$SFWREP"
+  sed s/"OUT="/' -i '/g | \
+  sed s/"SRC="/' -s '/ | \
+  sed s/"DST="/' -d '/g | \
+  sed s/"PROTO="/' -p '/g | \
+  sed s/"SPT="/' --sport '/g | \
+  sed s/"DPT="/' --dport '/g | \
+  sed s/"TYPE="/' --icmp-type '/g | \
+  xargs echo -n | \
+  sed s/"dport [0-9]*"/"&\n"/g | \
+  sed s/"^"/"\[0:0\] -A "/g | \
+  sed s/"TCP"/'tcp -m tcp'/g | \
+  sed s/"UDP"/'udp -m udp'/g | \
+  sed s/"ICMP"/'icmp -m icmp'/g | \
+  sed s/'\(--icmp-type [0-9]*\) \[.*'/"\1"/g | \
+  sed s/"  *"/" "/g | \
+  sort -k 4 -u > "$SFWREP"
 
 
 
